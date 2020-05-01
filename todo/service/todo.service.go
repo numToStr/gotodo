0f3838d05984f9050cc27349609e65662ddca695
numToStr <vikasraj1911@gmail.com>
2020-05-01 12:40:06 +0530

    change: folder structure

---
+++ todo/service/todo.service.go
@@ -0,0 +1,40 @@
+package service
+
+import "github.com/gofiber/fiber"
+
+// Todo defines the todo item
+type Todo struct {
+	Title string `json:"title"`
+}
+
+// Todos defines the todos list
+type Todos struct {
+	Todos []Todo `json:"todos"`
+}
+
+// GetTodos returns the todos list
+func GetTodos(c *fiber.Ctx) {
+	d := &Todos{
+		Todos: []Todo{
+			{
+				Title: "Hello",
+			},
+		},
+	}
+
+	c.JSON(d)
+}
+
+// GetTodo return a single todo
+func GetTodo(c *fiber.Ctx) {
+	todoID := c.Params("todoID")
+
+	c.Send(todoID)
+}
+
+// DeleteTodo deletes a single todo
+func DeleteTodo(c *fiber.Ctx) {
+	todoID := c.Params("todoID")
+
+	c.Send(todoID)
+}
--- todo/todo.go
+++ todo/todo.go
@@ -1,36 +1,16 @@
 package todo
 
-import "github.com/gofiber/fiber"
+import (
+	"numtostr/gofiber/todo/service"
 
-// Todo defines the todo item
-type Todo struct {
-	Title string `json:"title"`
-}
-
-// Todos defines the todos list
-type Todos struct {
-	Todos []Todo `json:"todos"`
-}
+	"github.com/gofiber/fiber"
+)
 
 // Routes contains all routes relative to /todo
 func Routes(app *fiber.App) {
 	r := app.Group("/todo")
 
-	r.Get("/list", func(c *fiber.Ctx) {
-		d := &Todos{
-			Todos: []Todo{
-				{
-					Title: "Hello",
-				},
-			},
-		}
-
-		c.JSON(d)
-	})
-
-	r.Get("/:todoID", func(c *fiber.Ctx) {
-		todoID := c.Params("todoID")
-
-		c.Send(todoID)
-	})
+	r.Get("/list", service.GetTodos)
+	r.Get("/:todoID", service.GetTodo)
+	r.Delete("/:todoID", service.DeleteTodo)
 }
