d8c2072c26b3d7a6342d5cacc028c15feec71710
numtostr <vikasraj1911@gmail.com>
2020-07-15 19:52:15 +0530

    fix: out of bound error in auth middleware

--- utils/middleware/authentication.go
+++ utils/middleware/authentication.go
@@ -16,7 +16,18 @@ func Auth(c *fiber.Ctx) {
 		return
 	}
 
-	user, err := jwt.Verify((strings.Split(h, " "))[1])
+	// Spliting the header
+	chunks := strings.Split(h, " ")
+
+	// If header signature is not like `Bearer <token>`, then throw
+	// This is also required, otherwise chunks[1] will throw out of bound error
+	if len(chunks) < 2 {
+		c.Next(fiber.ErrUnauthorized)
+		return
+	}
+
+	// Verify the token which is in the chunks
+	user, err := jwt.Verify(chunks[1])
 
 	if err != nil {
 		c.Next(fiber.ErrUnauthorized)
